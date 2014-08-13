49a384529c31eb5a1fe6174b20893a807cf8bc51
Antolis <josipantolis@gmail.com>
2014-08-13 08:10:02 +0200

    Starting with test for javascript engine

--- app/src/main/java/com/example/josip/jstest/MyActivity.java
+++ app/src/main/java/com/example/josip/jstest/MyActivity.java
@@ -2,6 +2,7 @@ package com.example.josip.jstest;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
