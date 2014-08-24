b5e3d62b89d4d6efaf67120dd059861e1d993c36
Antolis <josipantolis@gmail.com>
2014-08-24 23:20:36 +0200

    Using injected location manager in MyActivity

--- app/src/main/java/com/example/josip/jstest/activities/MyActivity.java
+++ app/src/main/java/com/example/josip/jstest/activities/MyActivity.java
@@ -2,6 +2,8 @@ package com.example.josip.jstest.activities;
 
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
+import android.location.Criteria;
+import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.Handler;
@@ -12,18 +14,16 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
-import com.example.josip.gameService.GameEngineService;
 import com.example.josip.QExample;
-import com.example.josip.jstest.JavaScriptEngine;
+import com.example.josip.gameService.GameEngineService;
 import com.example.josip.jstest.R;
 import com.mysema.query.sql.SQLQuery;
 import com.mysema.query.sql.SQLQueryImpl;
 import com.mysema.query.sql.SQLTemplates;
 import com.mysema.query.sql.SQLiteTemplates;
 
-//import org.flywaydb.core.Flyway;
-//import org.flywaydb.core.api.android.ContextHolder;
 import org.flywaydb.core.Flyway;
 import org.flywaydb.core.api.android.ContextHolder;
 import org.slf4j.Logger;
@@ -36,53 +36,29 @@ import java.util.Properties;
 
 import javax.inject.Inject;
 
-import static com.mysema.query.alias.Alias.$;
-import static com.mysema.query.alias.Alias.alias;
+//import org.flywaydb.core.Flyway;
+//import org.flywaydb.core.api.android.ContextHolder;
 
 public class MyActivity extends InjectionActivity {
 
     private static final Logger logger = LoggerFactory.getLogger(MyActivity.class);
-
     @Inject
     LocationManager locationManager;
+    private Button startServiceButton, stopServiceButton;
+    private TextView latitudeText, longitudeText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+        initViewComponents();
 
-        SQLiteDatabase db = openOrCreateDatabase("test", 0, null);
-        ContextHolder.setContext(this);
-        Flyway flyway = new Flyway();
-        flyway.setDataSource("jdbc:sqlite:" + db.getPath(), "", "");
-        flyway.migrate();
+        experimentWithDatabase();
 
-        Connection connection = null;
-        try {
-            connection = new SQLDroidDriver().connect("jdbc:sqlite:" + db.getPath() , new Properties());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        useLocationManager();
 
-        SQLTemplates dialect = new SQLiteTemplates(); // SQL-dialect
-        SQLQuery query = new SQLQueryImpl(connection, dialect);
-
-        QExample c = new QExample("example");
-        List<String> lastNames = query.from(c)
-                .where(c.name.eq("Bob"))
-                .list(c.name);
-        logger.info(lastNames.size()+"");
-
-        String MY_AWESOME_STRING = "'Awesome'";
-
-        final String CUSTOM_STRING_FUNCTION = "var onEnter = function (string) { return string += ' and then some!' };";
-
-        Log.d("JEJ", new JavaScriptEngine().runOnEnterScript(CUSTOM_STRING_FUNCTION, MY_AWESOME_STRING));
-        logger.debug("LOGGING JUHU");
-        logger.info("LOGGING INFO");
-
-        final Intent i= new Intent(this, GameEngineService.class);
-        Messenger messenger = new Messenger(new Handler(){
+        final Intent i = new Intent(this, GameEngineService.class);
+        Messenger messenger = new Messenger(new Handler() {
             @Override
             public void handleMessage(Message msg) {
                 super.handleMessage(msg);
@@ -91,23 +67,20 @@ public class MyActivity extends InjectionActivity {
         });
         i.putExtra("Messenger", messenger);
 
-        Button startButton = (Button) findViewById(R.id.startServiceButton);
-        startButton.setOnClickListener(new View.OnClickListener() {
+        startServiceButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startService(i);
             }
         });
 
-        Button stopButton = (Button) findViewById(R.id.stopServiceButton);
-        stopButton.setOnClickListener(new View.OnClickListener() {
+        stopServiceButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 stopService(i);
             }
         });
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -126,5 +99,48 @@ public class MyActivity extends InjectionActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void initViewComponents() {
+        startServiceButton = (Button) findViewById(R.id.startServiceButton);
+        stopServiceButton = (Button) findViewById(R.id.stopServiceButton);
+
+        latitudeText = (TextView) findViewById(R.id.latitude);
+        longitudeText = (TextView) findViewById(R.id.longitude);
+    }
+
+    private void experimentWithDatabase() {
+
+        SQLiteDatabase db = openOrCreateDatabase("test", 0, null);
+        ContextHolder.setContext(this);
+        Flyway flyway = new Flyway();
+        flyway.setDataSource("jdbc:sqlite:" + db.getPath(), "", "");
+        flyway.migrate();
+
+        Connection connection = null;
+        try {
+            connection = new SQLDroidDriver().connect("jdbc:sqlite:" + db.getPath(), new Properties());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        SQLTemplates dialect = new SQLiteTemplates(); // SQL-dialect
+        SQLQuery query = new SQLQueryImpl(connection, dialect);
+
+        QExample c = new QExample("example");
+        List<String> lastNames = query.from(c)
+                .where(c.name.eq("Bob"))
+                .list(c.name);
+        logger.info(lastNames.size() + "");
+
+    }
+
+
+    private void useLocationManager() {
+
+        Location location = locationManager.getLastKnownLocation(locationManager.getBestProvider(new Criteria(), false));
+        latitudeText.setText(Double.toString(location.getLatitude()));
+        longitudeText.setText(Double.toString(location.getLongitude()));
+
     }
 }
